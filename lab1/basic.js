e19e1db2a4f35764bb0dc7805bdc9aaa9e624d67
Nguyen Dinh Tran Long <longndt226@gmail.com>
2023-09-15 14:16:41 +0700

    lab1

---
+++ lab1/basic.js
@@ -0,0 +1,7 @@
+console.log("Hello World!");
+console.log("We are learning NodeJS")
+console.log('Today is a good day')
+console.log(2023)
+console.log(3.14)
+console.log(true)
+console.log("15/09/2023")
