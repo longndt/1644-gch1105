cc70d7b2c7538ad4a0fefbc39aae66455c37f5b1
Nguyen Dinh Tran Long <longndt226@gmail.com>
2023-09-15 14:35:47 +0700

    lab1

---
+++ lab1/variable.js
@@ -0,0 +1,43 @@
+//cách 1
+a = 3
+b = 5.5
+c = false
+d = "Greenwich Vietnam"
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+//cách 2: sử dụng "var" (có tác dụng ở toàn cục : global)
+var x = 5
+var y = 2.7
+console.log(x)
+
+//cách 3: sử dụng "let" (có tác dụng ở cục bộ : local)
+let n = 10
+let m = 20
+console.log(n)
+
+//cách 4: sử dụng "const"  (hằng số)
+const PI = 3.14
+console.log(PI)
+
+a = 1
+b = 3
+c = a + b
+console.log("a = " + a)
+console.log("b = " + b)
+console.log("a + b = " + c)
+console.log("a - b = " + (a-b))
+
+x = 17
+y = 5
+console.log(x/y)
+console.log(x%y)
+
+a = 10
+b = a++     //b=a=10 -> a++=a+1=10+1=11
+c = ++a     //++a=a+1=11+1=12 -> c=a=12
+console.log("a = " + a)  //12
+console.log("b = " + b)  //10
+console.log("c = " + c)  //12
